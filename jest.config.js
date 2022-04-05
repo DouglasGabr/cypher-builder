f687df50dc7080d2eb41afa667c090fc48b292b5
DouglasGabr <douglasgabriel99@gmail.com>
2022-04-05 16:41:39 -0300

    refactor(tests): use SWC as transpiler

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
   testMatch: ['**/*.spec.ts'],
   setupFilesAfterEnv: ['jest-extended/all'],
   collectCoverageFrom: ['./src/**/*.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+          },
+          target: 'es2021',
+        },
+        sourceMaps: 'inline',
+      },
+    ],
+  },
 };
 module.exports = config;
